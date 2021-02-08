71ef67858871e7fbaea63ef565915a52c66d9093
David Wright <davidjwright8@gmail.com>
2021-02-08 09:20:47 +0000

    Write new username to database and return ID

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser');
+const database = require('./database');
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,10 +11,18 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
-
+app.use(bodyParser.urlencoded({extended: false}));
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+app.post('/api/exercise/new-user', (req, res) => {
+  database.saveNewUserToDatabase(req.body.username, (err, response) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(response);      
+    }
+  });
+});
